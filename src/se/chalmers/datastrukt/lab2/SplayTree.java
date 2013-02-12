3aa4f7b50936a6ad13a7c0a0155c3d485fbea043
Tomas <tomsel1@hotmail.com>
2013-02-12 10:25:46 +0100

    created class SplayTree.java

---
+++ src/se/chalmers/datastrukt/lab2/SplayTree.java
@@ -0,0 +1,15 @@
+package se.chalmers.datastrukt.lab2;
+
+import testSortCol.CollectionWithGet;
+import datastructures.BinarySearchTree;
+
+public class SplayTree<E extends Comparable<? super E>> extends
+		BinarySearchTree<E> implements CollectionWithGet<E> {
+
+	@Override
+	public E get(E e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
